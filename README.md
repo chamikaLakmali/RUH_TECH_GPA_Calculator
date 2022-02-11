1361480e64251a863bdc98dd00f7018dd675e9cf
Chamika Lakmali <54311242+chamikaLakmali@users.noreply.github.com>
2022-02-11 20:02:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RUH_TECH_GPA_Calculator
+ GPA calcultor for faculty of Technology University of Ruhuna developd with java
---
+++ src/ruh_tech_gpa_calculator/RUH_TECH_GPA_Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ruh_tech_gpa_calculator;
+
+/**
+ *
+ * @author User01
+ */
+public class RUH_TECH_GPA_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
